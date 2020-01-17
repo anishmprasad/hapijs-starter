03c65f99e9eeb3b4dacd4a7070aa7f1e08e824bc
Anish.M.Prasad <anish.m.prasad@gmail.com>
2020-01-17 20:28:35 +0530

    refactor update

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-"use strict";
+'use strict';
 
-const Hapi = require("@hapi/hapi");
+const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: "localhost"
-  });
+	const server = Hapi.server({
+		port: 3000,
+		host: 'localhost'
+	});
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "Hello World!";
-    }
-  });
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: (request, h) => {
+			return 'Hello World!';
+		}
+	});
 
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
+	await server.start();
+	console.log('Server running on %s', server.info.uri);
 };
 
-process.on("unhandledRejection", err => {
-  console.log(err);
-  process.exit(1);
+process.on('unhandledRejection', err => {
+	console.log(err);
+	process.exit(1);
 });
 
 init();
